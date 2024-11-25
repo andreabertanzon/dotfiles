0d97758ba3d93c33c6bbab84096562ad7e8249f3
andrea.bertanzon <andrea.bertanzon@fairmat.com>
2024-11-25 12:32:23 +0100

    modifies dotfiles with new lazyvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,30 +18,33 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- Extra modules hereb
+    -- Extra modules here
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     -- import/override with your plugins
     { import = "plugins" },
+    -- Override nvim-cmp configuration to disable ghost-text
+    {
+      "hrsh7th/nvim-cmp",
+      opts = function(_, opts)
+        opts.experimental = opts.experimental or {}
+        opts.experimental.ghost_text = false
+      end,
+    },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+  },
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
         -- "matchit",
